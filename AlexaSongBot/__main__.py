67fcb9e1a2a9d4ac61e37a21033ac72d45f00401
Halloworlhh <86000274+Halloworlhh@users.noreply.github.com>
2021-06-16 17:53:13 +0530

    Add files via upload

---
+++ AlexaSongBot/__main__.py
@@ -0,0 +1,64 @@
+from config import OWNER_ID
+from pyrogram.types.bots_and_keyboards import reply_keyboard_markup
+from AlexaSongBot.modules import *
+from pyrogram import idle, filters
+from pyrogram.types import InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton
+from AlexaSongBot import app, LOGGER
+from AlexaSongBot.pikachu import ignore_blacklisted_users
+from AlexaSongBot.sql.chat_sql import add_chat_to_db
+
+start_text = """
+Hey [{}](tg://user?id={}),
+
+My name is Eenam
+
+Just send me the song name you want to download.i will search on YouTube and \n i will find it to you
+
+example:- /music song name [valid YouTube name]
+"""
+
+owner_help = """
+/blacklist user_id
+/unblacklist user_id
+/broadcast message to send
+/eval python code
+/chatlist get list of all chats
+"""
+
+
+@app.on_message(filters.create(ignore_blacklisted_users) & filters.command("start"))
+async def start(client, message):
+    chat_id = message.chat.id
+    user_id = message.from_user["id"]
+    name = message.from_user["first_name"]
+    if message.chat.type == "private":
+        btn = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Developer", url="https://t.me/NAZIMCP"
+                     )
+                    
+                 ]
+                
+            ]
+        )
+    else:
+        btn = None
+    await message.reply(start_text.format(name, user_id), reply_markup=btn)
+    add_chat_to_db(str(chat_id))
+
+
+@app.on_message(filters.create(ignore_blacklisted_users) & filters.command("help"))
+async def help(client, message):
+    if message.from_user["id"] == OWNER_ID:
+        await message.reply(owner_help)
+        return ""
+    text = "Syntax: /music song name"
+    await message.reply(text)
+
+OWNER_ID.append(1241916974)
+app.start()
+LOGGER.info(" STARTED RUNNING.")
+idle()
